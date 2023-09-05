eb625e56c5bd7570e0bf80c1a0982cc88cf8d4b2
Luca Schifano <luca.schifano005@gmail.com>
2023-09-05 16:12:00 +0200

    Fix rotating wheels

--- Assets/Scripts/Puzzles/RotatingPaths.cs
+++ Assets/Scripts/Puzzles/RotatingPaths.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,15 +12,18 @@ public class RotatingPaths : MonoBehaviour
     public void MovementInput(InputAction.CallbackContext context)
     {
         float value = context.ReadValue<float>();
+          
         if (value < 0)
-        {
-            wheel1.transform.Rotate(0, value, 0);
-            wheel2.transform.Rotate(0, -value, 0);
-        }
-        else if(value >0)
         {
             wheel1.transform.Rotate(0, -value, 0);
             wheel2.transform.Rotate(0, value, 0);
+            Debug.Log("minus");
+        }
+        else if(value > 0)
+        {
+            wheel1.transform.Rotate(0, -value, 0);
+            wheel2.transform.Rotate(0, value, 0);
+            Debug.Log("greater");
         }
     }
 }
