8259fca81f0840553c29ba0683051819d173ffa9
DanZo0x <dan.cheype@gmail.com>
2023-09-05 17:07:07 +0200

    [ADD] Pooling system began

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private BoxCollider _playerBoxCollider;
+    [SerializeField] private PlayerCollision _collision;
+
+    [Space]
+    [Header("Checks")]
+    [SerializeField] private bool canMove = true;
+    [SerializeField] private bool isJumping = false;
+
+    [Space]
+    [Header("Speed Values")]
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _speedModifier = 1f;
+    [SerializeField] private float _slowModifier = 1f;
+
+    [Space]
+    [Header("Jump Values")]
+    [SerializeField] private float _jumpForce;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _collision = GetComponent<PlayerCollision>();
+        _playerBoxCollider = GetComponent<BoxCollider>();
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        if (_collision.onGround)
+        {
+            isJumping = false;
+        }
+        else
+        {
+            isJumping = true;
+        }
+
+        if (canMove)
+        {
+            _speed += Time.deltaTime / 20;
+            _speed = Mathf.Clamp(_speed, 3, 4);
+
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * 4 * _speed / _slowModifier * _speedModifier;
+        }
+    }
+
+    private void Jump(Vector3 dir)
+    {
+        _rigidBody.velocity = new Vector3(_rigidBody.velocity.x, 0, 0);
+        _rigidBody.velocity += dir * _jumpForce;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && !isJumping)
+        {
+            _rigidBody.velocity = new Vector3(_rigidBody.velocity.x, 0, 0);
+            Jump(Vector3.up);
+        }
+    }
+}
