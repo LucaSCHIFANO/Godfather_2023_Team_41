935bfbe62ca114a34b1b7c972e3abd35d4e8aa13
Luca Schifano <luca.schifano005@gmail.com>
2023-09-05 16:05:21 +0200

    Rotating Wheels

---
+++ Assets/Scripts/Puzzles/RotatingPaths.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotatingPaths : MonoBehaviour
+{
+    [SerializeField] GameObject wheel1;
+    [SerializeField] GameObject wheel2;
+
+    public void MovementInput(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+        if (value < 0)
+        {
+            wheel1.transform.Rotate(0, value, 0);
+            wheel2.transform.Rotate(0, -value, 0);
+        }
+        else if(value >0)
+        {
+            wheel1.transform.Rotate(0, -value, 0);
+            wheel2.transform.Rotate(0, value, 0);
+        }
+    }
+}
